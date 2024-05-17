86d53700f5bd92a1017071933e5e003f64c8e876
franknmungai <frankmnganga@gmail.com>
2024-05-17 17:10:33 +0300

    fixed depl error

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import toast from 'react-hot-toast';
 
 const Home = () => {
